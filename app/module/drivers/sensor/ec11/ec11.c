dec2c2200e48e4de36f93da066c2e96f8842fad7
imhy123 <spirithy@gmail.com>
2026-03-03 14:23:18 +0800

    fix: include zephyr/sys_clock.h;

--- app/module/drivers/sensor/ec11/ec11.c
+++ app/module/drivers/sensor/ec11/ec11.c
@@ -8,7 +8,7 @@
 
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/clock.h>
+#include <zephyr/sys_clock.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/sensor.h>
